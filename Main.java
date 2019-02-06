8f32bedc13857dfffdfb41d4d9980ac9d064d458
toloro2001 <40397052+toloro2001@users.noreply.github.com>
2019-02-06 12:58:31 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.applet.*;
 import java.awt.*;
 
-public class Main extends MyApplet{
+public class Main extends Applet{
    public void paint(Graphics g){
       g.drawString("Welcome in Java Applet.",40,20);
    }
