7dd812d88e6febf09de9e557af7c94b21403444b
toloro2001 <40397052+toloro2001@users.noreply.github.com>
2019-02-06 11:27:30 -0500

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,79 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Game extends JFrame implements KeyListener {
+
+    private Board board;
+    private boolean upPressed, downPressed;
+
+    public Game(){
+
+        setResizable(false);
+        //************************
+        setVisible(true);
+        //************************
+        setFocusable(true);
+        setTitle("PONG");
+        //setIcon is also a property for your own discovery and usage
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        board = new Board(this);
+
+        add(board);
+        addKeyListener(this);
+
+        pack();
+        //Sets the frame in the middle of the screen
+        setLocationRelativeTo(null);
+        board.init();
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+            upPressed = true;
+        }
+
+        if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            downPressed = true;
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+            upPressed = false;
+        }
+
+        if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            downPressed = false;
+        }
+
+    }
+
+    public boolean isUpPressed(){
+        return upPressed;
+    }
+
+    public boolean isDownPressed(){
+        return downPressed;
+    }
+
+    public static void main(String[] args){
+        new Game();
+    }
+
+}
+
+
