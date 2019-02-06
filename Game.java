15f0ab3ca2c7b564e94ad83048fc9c2c79e429f5
toloro2001 <40397052+toloro2001@users.noreply.github.com>
2019-02-06 12:31:16 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -76,14 +76,3 @@ public class Game extends JFrame implements KeyListener {
     }
   
 }  
-import java.applet.*;
-import java.awt.*;
-
-public class Main extends Applet{
-   public void paint(Graphics g){
-      g.drawString("Welcome in Java Applet.",40,20);
-   }
-}
-
-
-
