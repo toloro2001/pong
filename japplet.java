b04a38cc8a702133c51666fa6d1cd7724d5a9466
toloro2001 <40397052+toloro2001@users.noreply.github.com>
2019-02-06 12:50:56 -0500

    Update japplet.java

--- japplet.java
+++ japplet.java
@@ -1,19 +1,5 @@
-public class MyApplet extends JApplet {
-
-   @Override
-   public void init() {
-      try {
-         SwingUtilities.invokeAndWait(new Runnable() {
-            public void run() {
-               JFrame myFrame = new MyFrame("Frame");
-               Container contentPane = myFrame.getContentPane();
-               setContentPane(contentPane);
-            }
-         });
-      } catch (InterruptedException e) {
-         e.printStackTrace();
-      } catch (InvocationTargetException e) {
-         e.printStackTrace();
-      }
-   }
+public class MyApplet extends Applet {
+    public void start() {
+         MyApplication.main(new String[0]);
+    }
 }
